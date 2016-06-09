70b2c630602031e6c4aedc93ea541c563326fb15
Raphael Negrisoli Batista <raphael.batista@payu.com.br>
2016-06-09 12:05:52 -0300

    Add Angry Professor solution.

---
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/AngryProfessor.scala
@@ -0,0 +1,17 @@
+package com.negrisoli.algorithms.implementation
+
+object AngryProfessor {
+  
+  def main(args:Array[String]) {
+    val sc = new java.util.Scanner (System.in);
+    val nTestCases = sc.nextInt()
+    
+    for (i <- 0 to nTestCases-1) {
+      val nStudents = sc.nextInt()
+      val threshold = sc.nextInt()
+      val inTime = (0 to nStudents-1).map(i => sc.nextInt()).count(_ <= 0)
+      
+      println(if (inTime < threshold) "YES" else "NO")
+    }
+  }
+}
