d54e0bd5a8015512fc331dfc0b8557e9412f6600
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-06-10 18:50:50 -0300

    Add sherlockandthebeast.

---
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/SherlockandTheBeast.scala
@@ -0,0 +1,30 @@
+package com.negrisoli.algorithms.implementation
+
+object SherlockandTheBeast {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    val nCases = sc.nextInt();
+
+    for (i <- (0 to nCases - 1)) {
+      val digits = sc.nextInt()
+      val x = findMaxDiv(digits)
+      if ((digits - x) % 3 == 0) {
+        println("5" * digits)
+      } else if ((digits - x) % 5 == 0) {
+        println("5" * x + "3" * (digits - x))
+      } else {
+        println(-1)
+      }
+    }
+  }
+
+  def findMaxDiv(digits: Int): Int = {
+    for (i <- (digits to 3 by -5)) {
+      if (i % 3 == 0) return i
+    }
+
+    return 0
+  }
+
+}
