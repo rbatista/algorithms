734eb12d2a58ea0c0ee135a9581b288ad540c84a
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-06-11 11:28:20 -0300

    Add find digits solution.

---
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/FindDigits.scala
@@ -0,0 +1,25 @@
+object FindDigits {
+
+    def main(args:Array[String]) {
+        val sc = new java.util.Scanner(System.in)
+        val nTests = sc.nextInt
+        
+        for (i <- 0 to nTests - 1) {
+            val n = sc.nextInt
+            println(countDivisor(n))
+        }
+    }
+
+    def countDivisor(n:Int): Int = {
+        val digits = scala.collection.mutable.ArrayBuffer.empty[Int]
+        
+        var number = n
+        while (number > 0) {
+            digits += number % 10
+            number = number / 10
+        }
+        
+        return digits.filter( _ > 0).count(n % _ == 0)
+    }
+
+}
