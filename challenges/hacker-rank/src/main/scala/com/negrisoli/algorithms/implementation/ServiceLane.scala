c18aa5fe0fa6404c7b5db57ec06325572eb9ed58
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-06-14 17:26:36 -0300

    Add Servicelane solution.

---
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/ServiceLane.scala
@@ -0,0 +1,18 @@
+package com.negrisoli.algorithms.implementation
+
+object ServiceLane {
+  
+  def main(args:Array[String]) {
+    val sc = new java.util.Scanner(System.in);
+    val size = sc.nextInt();
+    val nTests = sc.nextInt();
+    
+    val freeway = (0 to size-1).map(i => sc.nextInt())
+    for (i <- (0 to nTests)) {
+      val enter = sc.nextInt()
+      val exit = sc.nextInt()
+      
+      println(freeway.slice(enter, exit + 1).min)
+    }
+  }
+}
