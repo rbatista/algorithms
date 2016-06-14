284cac7996bdbdf049320198694f0a417a7f847a
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-06-14 18:25:35 -0300

    Add Cutthesticks solution.

---
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/CutTheSticks.scala
@@ -0,0 +1,22 @@
+package com.negrisoli.algorithms.implementation
+
+object CutTheSticks {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in);
+    val size = sc.nextInt();
+
+    cut((0 to size-1).map(i => sc.nextInt()).toList)
+  }
+  
+  def cut(args:List[Int]) {
+    def iter(args:List[Int]) {
+      if (!args.isEmpty) {
+        println(args.size)
+        iter(args.map(_ - args.min).filter(_ > 0))
+      }
+    }
+    
+    iter(args)
+  }
+}
