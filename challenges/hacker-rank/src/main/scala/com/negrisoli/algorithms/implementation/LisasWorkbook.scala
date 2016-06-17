74f57ff53b9c432daea7bd4964466f9d1a3f4924
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-06-17 19:00:11 -0300

    Add lisa's workbook.

---
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/LisasWorkbook.scala
@@ -0,0 +1,26 @@
+package com.negrisoli.algorithms.implementation
+
+object LisasWorkbook {
+  
+  def main(args: Array[String]): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val size = sc.nextInt
+    val perPage = sc.nextInt
+    val problems = List.tabulate(size)(n => sc.nextInt)
+    
+    println(countSpecials(perPage, problems))
+  }
+  
+  def page(problems:Int, size:Int) = problems / size + (if (problems % size > 0) 1 else 0)
+  
+  def countSpecials(perPage:Int, problems:List[Int]):Int = {
+    val pages = problems.map(page(_, perPage)).sum
+    
+    val book = (0 to problems.size -1).flatMap(i =>
+       (1 to problems(i)).toList.sliding(3,3).toList
+     ).toList
+  
+     (1 to pages).count(i => book(i-1).contains(i)) 
+  }
+  
+}
