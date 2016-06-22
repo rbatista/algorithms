57808e1720e31c08db63309aa24148c55c4b443f
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-06-22 09:21:09 -0300

    Add the solution of the grid search, but need to be improved.

--- challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/TheGridSearch.scala
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/TheGridSearch.scala
@@ -2,23 +2,51 @@ package com.negrisoli.algorithms.implementation
 
 object TheGridSearch {
   
-  def readMatrix(rows: Int, cols: Int, p:() => String ): Array[Array[Int]] = {
+  type Row = Array[Int]
+  type Matrix = Array[Row]
+  
+  def readMatrix(rows: Int, cols: Int, p:() => String ): Matrix = {
     p() // clean \n
     Array.range(0, rows).map(r => lineToArray(p()))
   }
   
-  def lineToArray(s:String): Array[Int] = {
+  def lineToArray(s:String): Row = {
     s.sliding(1, 1).map(_.toInt).toArray
   }
   
-  def indexOffAll(line:Array[Int], pattern:Array[Int]) = {
-    def iter(acc:Array[Int], pos: Int, patPos: Int): Array[Int] =
-      if (line.size - pos < pattern.size - patPos) acc
-      else if (patPos >= pattern.size) acc ++ Array(pos - pattern.size) 
-      else if (line(pos) == pattern(patPos)) iter(acc, pos + 1, patPos + 1) ++ iter(acc, pos + 1, patPos)
+  def indexOffAll(line:Row, pattern:Row) = {
+    val arr = scala.collection.mutable.ArrayBuffer.empty[Int]
+    
+    def iter(acc:Array[Int], pos: Int, patPos: Int): scala.collection.mutable.ArrayBuffer[Int] =
+      if (line.size - pos < pattern.size - patPos) arr
+      else if (patPos >= pattern.size) arr += (pos - pattern.size) 
+      else if (line(pos) == pattern(patPos)) {
+        iter(acc, pos + 1, patPos + 1)
+        iter(acc, pos + 1, patPos)
+        arr
+      }
       else iter(acc, pos + 1, 0)
    
-    iter(Array[Int](), 0, 0)
+    iter(Array[Int](), 0, 0).toArray
+  }
+  
+  def matchPattern(matrix:Matrix, pattern:Matrix):Boolean = {
+    def isPatternAt(mRow:Int, pRow:Int, cIndex:Int):Boolean = {
+      if (pRow >= pattern.size) return true
+      else if (mRow >= matrix.size) return false
+      
+      val lineMatch = indexOffAll(matrix(mRow), pattern(pRow))
+      if (lineMatch.contains(cIndex)) isPatternAt(mRow + 1, pRow + 1, cIndex)
+      else false
+    }
+    
+    for (i <- 0 to matrix.size-1) {
+      val lineMatches = indexOffAll(matrix(i), pattern(0));
+      for (j <- lineMatches) {
+        if (isPatternAt(i + 1, 1, j)) return true
+      }
+    }
+    false
   }
 
   def main(args: Array[String]): Unit = {
@@ -28,8 +56,7 @@ object TheGridSearch {
       val matrix = readMatrix(sc.nextInt(), sc.nextInt(), () => sc.nextLine())
       val pattern = readMatrix(sc.nextInt(), sc.nextInt(), () => sc.nextLine())
       
-      println("mat" + matrix(0))
-      println("pat" + pattern(0))
+      println(if (matchPattern(matrix, pattern)) "YES" else "NO")
     }
   }
   
--- challenges/hacker-rank/src/test/scala/com/negrisoli/algorithms/implementation/TheGridSearchTest.scala
+++ challenges/hacker-rank/src/test/scala/com/negrisoli/algorithms/implementation/TheGridSearchTest.scala
@@ -34,4 +34,54 @@ class TheGridSearchTest extends FunSuite with BeforeAndAfter {
     assert(indexOffAll(Array(0, 1, 2, 1, 2, 5), Array(1,2)) === Array(1, 3))
   }
   
+  
+  trait MatrixTest {
+    val mat0: Matrix = Array(Array(0, 1, 2), Array(3, 4, 5), Array(6, 7, 8))
+    val pat0: Matrix = Array(Array(1, 2), Array(4, 5))
+    val pat0_0: Matrix = Array(Array(0, 1), Array(4, 5))
+    val pat0_1: Matrix = Array(Array(4, 5), Array(7, 8))
+    val mat1: Matrix = Array(Array(9, 9, 9), Array(1, 2, 3), Array(5, 5, 5))
+    val pat1: Matrix = Array(Array(9, 9), Array(1, 2), Array(5, 5))
+    
+    val mat2: Matrix = Array(Array(1, 2, 3, 4, 1, 2), Array(5, 6, 1, 2, 1, 2),
+        Array(1, 2, 3, 6, 3, 4), Array(7, 8, 1, 2, 8, 8))
+    val pat2: Matrix = Array(Array(1, 2), Array(3, 4))
+    
+  }
+  
+  test("matchPattern find mat0") {
+    new MatrixTest {
+      assert(matchPattern(mat0, pat0) == true)
+    }
+  }
+  
+  test("matchPattern find mat1") {
+    new MatrixTest {
+      assert(matchPattern(mat1, pat1) == true)
+    }
+  }
+  
+  test("matchPattern not find mat0") {
+    new MatrixTest {
+      assert(matchPattern(mat0, pat1) == false)
+    }
+  }
+  
+  test("matchPattern not match mat0 deslocated pattern 0_0") {
+    new MatrixTest {
+      assert(matchPattern(mat0, pat0_0) == false)
+    }
+  }
+  
+  test("matchPattern match mat0 on second line") {
+    new MatrixTest {
+      assert(matchPattern(mat0, pat0_1) == true)
+    }
+  }
+  
+  test("matchPattern match mat2") {
+    new MatrixTest {
+      assert(matchPattern(mat2, pat2) == true)
+    }
+  }
 }
