5611aef61b35ea6e3de183692b41718963f69dc5
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-06-29 11:38:56 -0300

    Add a Caesar cipher solution.

---
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/CaesarCipher.scala
@@ -0,0 +1,23 @@
+package com.negrisoli.algorithms.implementation
+
+object CaesarCipher {
+
+    def main(args: Array[String]) {
+        val sc = new java.util.Scanner (System.in);
+        var n = sc.nextInt();
+        var s = sc.next();
+        var k = sc.nextInt();
+        
+        s.map(_.toInt).map(x => cripto(x, k)).map(x => print(x.toChar))
+    }
+           
+    def cripto(c:Int, k:Int):Int  = {
+        if (isUper(c)) (c-65 + k) % 26 + 65
+        else if (isDown(c)) (c-97 + k)  % 26 + 97
+        else c
+    } 
+               
+    def isUper(c:Int) = c >= 65 && c <= 90
+    def isDown(c:Int) = c >= 97 && c <= 122
+
+}
