15aa8bb9e3d1fc3b38ee8317db1ae48f5d24499e
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-07-07 10:00:55 -0300

    Fix equal stacks solution.

--- challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/EqualStacks.scala
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/EqualStacks.scala
@@ -9,18 +9,18 @@ object EqualStacks {
     val s2 = 0.to(n2 - 1).map( i => sc.nextInt() ).toList
     val s3 = 0.to(n3 - 1).map( i => sc.nextInt() ).toList
     
-    println(resolve(s1, s2, s3))
-  }
-
-  def resolve(s1: List[Int], s2: List[Int], s3: List[Int]): Int = {
     val h1 = s1.sum
     val h2 = s2.sum
     val h3 = s3.sum
     
+    println(resolve(s1, h1, s2, h2, s3, h3))
+  }
+
+  def resolve(s1: List[Int], h1: Int, s2: List[Int], h2: Int, s3: List[Int], h3:Int): Int = {
     if (h1 == h2 && h2 == h3) h1
-    else if (h1 > h2 && h1 > h3) resolve(s1.tail, s2, s3)
-    else if (h2 > h1 && h2 > h3) resolve(s1, s2.tail, s3)
-    else resolve(s1, s2, s3.tail)
+    else if (h1 >= h2 && h1 >= h3) resolve(s1.tail, h1 - s1.head, s2, h2, s3, h3)
+    else if (h2 >= h1 && h2 >= h3) resolve(s1, h1, s2.tail, h2 - s2.head, s3, h3)
+    else resolve(s1, h1, s2, h2, s3.tail, h3 - s3.head)
   }
   
 }
