63dc6a0cccb14676c78c9df1ddc77a61352acbac
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-07-26 10:49:08 -0300

    Add jumping in the clouds revisited solution.

---
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/JumpingInTheCloudsRevisited.scala
@@ -0,0 +1,17 @@
+/* https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited */
+package com.negrisoli.algorithms.implementation
+
+object JumpingInTheCloudsRevisited {
+  
+  def main(args: Array[String]): Unit = {
+    val sc = new java.util.Scanner(System.in)
+    val List(n, k) = (0 to 1) map (i => sc.nextInt) toList
+    val clouds = (0 to n-1) map (i => sc.nextInt) toList
+    
+    println(calcEnergy(k, clouds))
+  }
+
+  def calcEnergy(k: Int, clouds: List[Int]): Int = {
+    100 - (((0 to clouds.size - 1 by k) count (i => clouds(i) == 1)) * 2 + clouds.size / k)
+  }
+}
