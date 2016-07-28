cb80a58105c8301a74085c2fb1f4e2ecc38f5604
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2016-07-28 15:13:32 -0300

    Improve fair rations solution.

--- challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/FairRations.scala
+++ challenges/hacker-rank/src/main/scala/com/negrisoli/algorithms/implementation/FairRations.scala
@@ -8,31 +8,15 @@ object FairRations {
     val n = sc.nextInt
     val line = ((1 to n) map (i => sc.nextInt)).toList
 
-    println(distributeRations(line))
+    println(calcRations(line).getOrElse("NO"))
+  }
+  
+  def calcRations(xs: List[Int]): Option[Int] = {
+    val ys = xs.zipWithIndex filter (_._1 % 2 != 0)
+    if (ys.size % 2 == 1) None
+    else Some((ys map (_._2) grouped (2) map {
+      case List(x, y) => y - x
+    }).sum * 2)
   }
 
-  def distributeRations(line: List[Int]): String = {
-    def resolve: Int = {
-      val xs = line map { _ % 2 }
-      val sum = xs.sum
-      if (sum == 0) 0
-      else if (sum % 2 == 1) -1
-      else calcRations(xs)
-    }
-
-    resolve match {
-      case x if x >= 0 => x.toString()
-      case _           => "NO"
-    }
-  }
-
-  def calcRations(xs: List[Int]) = {
-    (xs.zipWithIndex.map {
-        case (v, i) =>
-          if (v == 0) -1
-          else i
-      } filter (_ != -1) sliding (2, 2) map {
-        case List(x, y) => y - x
-      }).sum * 2
-  }
 }
