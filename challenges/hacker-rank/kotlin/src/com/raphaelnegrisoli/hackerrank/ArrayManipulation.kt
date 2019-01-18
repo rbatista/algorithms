2add3734cd9ab7caef3c0efe3b75754aebd0400f
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2019-01-18 12:31:27 -0200

    Add ArrayManipulation solution

--- challenges/hacker-rank/kotlin/src/com/raphaelnegrisoli/hackerrank/ArrayManipulation.kt
+++ challenges/hacker-rank/kotlin/src/com/raphaelnegrisoli/hackerrank/ArrayManipulation.kt
@@ -5,13 +5,11 @@ package com.raphaelnegrisoli.hackerrank
 
 import java.util.*
 
-fun main(args: Array<String>) {
 
-    val (_, operations) =  readLine().orEmpty().trim()
-        .split(" ")
-        .map { it.toInt() }
-        .zipWithNext()
-        .first()
+/**
+ * Solution in {@sample m^2 + m}
+ */
+fun treeSolution(operations: Int) {
 
     val tree = TreeMap<Long, Long>()
     tree[1L] = 0
@@ -31,8 +29,41 @@ fun main(args: Array<String>) {
 
         // TODO optimize this
         tree.subMap(begin + 1, end + 1).replaceAll { _, v -> v + value }
-
     }
 
     println(tree.maxBy { it.value } ?.value ?: 0)
+}
+
+/**
+ * Solution in 2 * m
+ */
+fun main(args: Array<String>) {
+
+    val (size, operations) =  readLine().orEmpty().trim()
+        .split(" ")
+        .map { it.toInt() }
+        .zipWithNext()
+        .first()
+
+
+    val array = TreeMap<Int, Long>()
+
+    for (i in 0 until operations) {
+
+        val (begin, end, value) = readLine().orEmpty().trim()
+            .split(" ")
+
+        array[begin.toInt() - 1] = (array[begin.toInt() - 1] ?: 0) + value.toLong()
+        array[end.toInt()] = (array[end.toInt()] ?: 0) - value.toLong()
+
+    }
+
+    var max = 0L
+    var current = 0L
+    array.forEach {
+        current += it.value
+        if (current > max) max = current
+    }
+
+    println(max)
 }
