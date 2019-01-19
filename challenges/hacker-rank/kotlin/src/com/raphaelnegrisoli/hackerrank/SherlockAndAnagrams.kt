4383b41f3cec0e0b9c4f65a16815dadca463af71
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2019-01-19 10:15:48 -0200

    Add solotion for Sherlock and Anagrams problem

---
+++ challenges/hacker-rank/kotlin/src/com/raphaelnegrisoli/hackerrank/SherlockAndAnagrams.kt
@@ -0,0 +1,39 @@
+package com.raphaelnegrisoli.hackerrank
+
+import java.util.*
+
+inline fun String.sort(): String {
+
+    val charArray = this.toCharArray()
+    Arrays.sort(charArray)
+    return String(charArray)
+}
+
+fun sherlockAndAnagrams(word: String): Int {
+
+    val counts = hashMapOf<String, Int>()
+    for (s in 0 .. word.length - 2) {
+
+        for (i in 0 until word.length - s) {
+
+            val substring = (word.substring(i .. i + s)).sort()
+            counts[substring] = (counts[substring] ?: 0) + 1
+        }
+    }
+
+    return counts.map { it.value }
+        .map { it * (it - 1) / 2 }
+        .reduce(Int::plus)
+}
+
+fun main(args: Array<String>) {
+
+    val queries = readLine().orEmpty().trim().toInt()
+
+    for (q in 0 until queries) {
+
+        val word = readLine().orEmpty()
+        val result = sherlockAndAnagrams(word)
+        println(result)
+    }
+}
