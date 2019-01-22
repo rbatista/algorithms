016bd1549461f39f05d5d623a6b43f92a8515683
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2019-01-22 12:40:07 -0200

    Fix integer division at Count Triplets

--- challenges/hacker-rank/kotlin/src/com/raphaelnegrisoli/hackerrank/CountTriplets.kt
+++ challenges/hacker-rank/kotlin/src/com/raphaelnegrisoli/hackerrank/CountTriplets.kt
@@ -11,57 +11,39 @@ fun countTriplets(arr: List<Long>, ratio: Long): BigInteger {
     var triplets = BigInteger.ZERO
     for (i in arr) {
 
-        println("i=$i")
+        if (i % ratio == 0L) {
+            val previous = i / ratio
 
-        val previous = i / ratio
-        val previousDoublet = (previous / ratio) to previous
-        println(previousDoublet)
-        if ((doublets[previousDoublet] ?: BigInteger.ZERO) > BigInteger.ZERO) {
+            val previousDoublet = (previous / ratio) to previous
+            if ((doublets[previousDoublet] ?: BigInteger.ZERO) > BigInteger.ZERO) {
 
-            val value = doublets[previousDoublet]!!
-            println("plus ${value} to triplet (${previousDoublet.first}, ${previousDoublet.second}, $i) ")
-            triplets += value
+                triplets += doublets[previousDoublet]!!
+            }
+
+            val previousValue = singlets[previous] ?: BigInteger.ZERO
+            if (previousValue > BigInteger.ZERO) {
+
+                val doublet = previous to i
+                doublets[doublet] = (doublets[doublet] ?: BigInteger.ZERO) + previousValue
+            }
         }
 
-        val previousValue = singlets[previous] ?: BigInteger.ZERO
-        if (previousValue > BigInteger.ZERO) {
-
-            val doublet = previous to i
-            val value = (doublets[doublet] ?: BigInteger.ZERO) + previousValue
-            println("Update doublet $doublet = $value")
-            doublets[doublet] = value
-        }
-
-        val value = (singlets[i] ?: BigInteger.ZERO) + BigInteger.ONE
-        println("Update singlet $i = $value")
-        singlets[i] = value
-
-        println(singlets)
-        println(doublets)
-        println()
+        singlets[i] = (singlets[i] ?: BigInteger.ZERO) + BigInteger.ONE
     }
-
-    println(triplets)
 
     return triplets
 }
 
 fun main(args: Array<String>) {
 
-//    val (_, ratio) = readLine().orEmpty().trim()
-//        .split(" ")
-//        .map { it.toLong() }
-//    val arr = readLine().orEmpty().trim()
-//        .split(" ")
-//        .map { it.toLong() }
+    val (_, ratio) = readLine().orEmpty().trim()
+        .split(" ")
+        .map { it.toLong() }
+    val arr = readLine().orEmpty().trim()
+        .split(" ")
+        .map { it.toLong() }
 
-    for (i in listOf(Triple(listOf<Long>(1, 2, 1, 2, 4), 2L, 3),
-        Triple(listOf<Long>(1, 2, 4, 1, 2), 2L, 1),
-        Triple(listOf<Long>(1, 1, 1), 1L, 1))) {
 
-        println(countTriplets(i.first, i.second).toInt() == i.third)
-    }
-    //val result = countTriplets(arr, ratio)
-//    val result = countTriplets(arr, ratio)
-    //println(result)
+    val result = countTriplets(arr, ratio)
+    println(result)
 }
