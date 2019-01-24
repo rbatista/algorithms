6512d9bacc964355cd6d822a35130c90ad5caa4d
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2019-01-24 12:19:39 -0200

    Add StringComparator solution

---
+++ challenges/hacker-rank/kotlin/src/main/kotlin/com/raphaelnegrisoli/hackerrank/StringComparator.kt
@@ -0,0 +1,28 @@
+/**
+ * https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem
+ */
+package com.raphaelnegrisoli.hackerrank
+
+data class Player(val name: String, val score: Int)
+
+fun main(args: Array<String>) {
+
+    val n = readLine()!!.trim().toInt()
+
+    val players = arrayListOf<Player>()
+    for (i in 0 until n) {
+
+        val (name, score) = readLine()!!.trim().split(" ")
+        players.add(Player(name, score.toInt()))
+    }
+
+    players.sortWith(Comparator { p1, p2 ->
+        when {
+            p1.score < p2.score -> 1
+            p1.score > p2.score -> -1
+            else -> p1.name.compareTo(p2.name)
+        }
+    })
+
+    println(players.joinToString(separator = "\n") { p -> "${p.name} ${p.score}" })
+}
