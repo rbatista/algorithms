f70a8f10d720684ace527b2431fd3c63ebb2547d
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2019-01-29 16:23:50 -0200

    Add AlternatingCharacteres solution

---
+++ challenges/hacker-rank/kotlin/src/main/kotlin/com/raphaelnegrisoli/hackerrank/AlternatingCharacters.kt
@@ -0,0 +1,28 @@
+package com.raphaelnegrisoli.hackerrank
+
+
+fun main(args: Array<String>) {
+
+    val q = readLine()!!.trim().toInt()
+
+    for (i in 0 until q) {
+
+        val str = readLine()?.trim() ?: ""
+
+        var lastChar = if (str.isNotEmpty()) str[0] else ' '
+        var deletions = 0
+        for (char in str.drop(1)) {
+
+            if (char == lastChar) {
+
+                deletions++
+            } else {
+
+                lastChar = char
+            }
+        }
+
+        println(deletions)
+    }
+
+}
